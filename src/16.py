d493e77b5fb103d63f86101f6e820a21a2fb9f1c
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2022-12-18 10:20:23 -0500

    Almost works

--- src/16.py
+++ src/16.py
@@ -2,6 +2,9 @@
 
 import re
 import sys
+from typing import Generator, TypeVar
+
+T = TypeVar("T")
 
 
 def parse_input(input_txt: str):
@@ -59,13 +62,103 @@ def part1(input_txt: str) -> int:
     return brute_force(rates, tunnels, 30, "AA", set())
 
 
+def dijkstra(
+    vertices: set[T], edges: dict[T, set[T]], costs: dict[tuple[T, T], int], start: T
+) -> dict[T, tuple[T, int]]:
+    # Thank you Wikipedia
+
+    if start not in vertices:
+        raise ValueError("invalid starting vertex")
+
+    dist = {v: 999 for v in vertices}
+    prev: dict[T, T | None] = {v: None for v in vertices}
+
+    dist[start] = 0
+    unvisited = {v for v in vertices}
+
+    while unvisited:
+        u = min(unvisited, key=lambda u: dist[u])
+        unvisited.remove(u)
+
+        for v in edges[u]:
+            if v in unvisited:
+                alt = dist[u] + costs[u, v]
+                if alt < dist[v]:
+                    dist[v] = alt
+                    prev[v] = u
+
+    return {v: (p, dist[v]) for v, p in prev.items() if p is not None}
+
+
 def part2(input_txt: str) -> int:
-    ...
+    flow_rates, tunnels = parse_input(input_txt)
+
+    vertices = {valve for valve in flow_rates.keys()}
+    edges = {v1: tunnels[v1] for v1 in vertices}
+    costs: dict[tuple[str, str], int] = {
+        (v1, v2): 1 for v1, neighbors in edges.items() for v2 in neighbors
+    }
+
+    the_matrix = {
+        (v1, v2): cost
+        for v1 in vertices
+        for v2, (_, cost) in dijkstra(vertices, edges, costs, v1).items()
+        if (flow_rates[v1] > 0 or v1 == "AA") and flow_rates[v2] > 0
+    }
+    good_valves = {v for v in vertices if flow_rates[v] > 0}
+
+    def get_possible_paths(
+        valves: set[str], start: str, time_left: int, visited: set[str]
+    ) -> Generator[list[str], None, None]:
+        # create new set, do not update in-place!
+        visited = visited | {start}
+
+        reachable = {
+            v
+            for v in valves
+            if v not in visited and (the_matrix[start, v] + 1) <= time_left
+        }
+
+        if reachable:
+            for v in reachable:
+                for path in get_possible_paths(
+                    valves, v, time_left - (the_matrix[start, v] + 1), visited
+                ):
+                    yield [start] + path
+        else:
+            yield [start]
+
+    def get_path_reward(path: list[str], time: int) -> int:
+        total = 0
+        for i in range(len(path) - 1):
+            v1 = path[i]
+            v2 = path[i + 1]
+            time -= the_matrix[v1, v2] + 1
+            total += flow_rates[v2] * time
+        return total
+
+    t = 26
+    possible_paths = list(get_possible_paths(good_valves, "AA", t + 1, set()))
+
+    # print(len(possible_paths))
+
+    solution = 0
+    for path1 in possible_paths:
+        for path2 in possible_paths:
+            a = set(path1[1:])
+            b = set(path2[1:])
+            if not a & b:
+                print(a, b)
+                solution = max(
+                    solution, get_path_reward(path1, t) + get_path_reward(path2, t)
+                )
+
+    return solution
 
 
 def main():
     input_txt = sys.stdin.read()
-    print(part1(input_txt))
+    # print(part1(input_txt))
     print(part2(input_txt))
 
 
